03b560c5b94ddba16f7070994e3e51ffcb198be3
Jared Amaral <jaamaral@chapman.edu>
2023-11-12 18:50:51 -0800

    react template created

---
+++ carvenger/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='findCars'>
+      <button className='findCarButton'>Search for Cars</button>
+    </div>
+
+
+  )
+}
